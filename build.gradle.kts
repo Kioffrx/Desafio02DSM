a52897dce224677991d0762f4582b08cfa01becf
Kioffrx <entiquezometa@gmail.com>
2026-09-10 23:28:53 -0600

    agregar: configuracion inicial de Gradle del proyecto

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file donde se declaran las versiones de los plugins
+// usados por los distintos módulos del proyecto AeroGO.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    // Se habilitará en el siguiente paso, cuando conectemos Firebase:
+    // id("com.google.gms.google-services") version "4.4.2" apply false
+}
